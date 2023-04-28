df34c109c29460cb08f63c309a772b80f31b9659
orelmoshe <orel@spectory.com>
2023-04-28 06:18:26 +0300

    multi-threading_demo

---
+++ index_one_workers.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/non-blocking/', (req, res) => {
+	res.status(200).send('This page is non-blocking');
+});
+
+function calculateCount() {
+	return new Promise((resolve, reject) => {
+		let counter = 0;
+		for (let i = 0; i < 20_000_000_000; i++) {
+			counter++;
+		}
+		resolve(counter);
+	});
+}
+
+app.get('/blocking', async (req, res) => {
+	console.time();
+	const counter = await calculateCount();
+	console.timeEnd();
+	res.status(200).send(`result is ${counter}`);
+});
+
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`);
+});
